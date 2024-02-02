5d5f67ad8204e71b48775ac56dcb8e1d15159d9f
Layton <caolei6767@gmail.com>
2024-02-02 21:20:57 +0800

    update neovim update

--- tools/update_neovim.sh
+++ tools/update_neovim.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+script_dir=$(cd $(dirname $0) && pwd)
+
+cd $script_dir
+
 rm -rf ./nvim-linux64
 
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
@@ -9,6 +13,8 @@ tar -vxf nvim-linux64.tar.gz
 rm -rf nvim-linux64.tar.gz
 
 if [ "$#" -eq 1 ]; then
-	echo 'export PATH=$PATH:'"$(pwd)"/nvim-linux64/bin/ >>~/.exports
+	echo 'export PATH='"$script_dir"/nvim-linux64/bin/':$PATH' >>~/.exports
 	echo "Aleady append PATH env to ~/.exports"
 fi
+
+cd -
